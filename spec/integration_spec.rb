10ad58460412b2a2bac15725b49d2f0ad0b2a3df
Tommaso Barbugli <tbarbugli@gmail.com>
2014-05-30 22:14:14 +0200

    add bit more tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,16 +2,23 @@ require 'stream'
 
 describe "Integration tests" do
 
-    context 'init a test client' do
+    before do
+        @client = Stream::Client.new('5crf3bhfzesn', 'tfq2sdqpj9g446sbv653x3aqmgn33hsn8uzdc9jpskaw8mj6vsnhzswuwptuj9su')
+        @feed42 = @client.feed('flat:42')
+        @test_activity = {:actor => 1, :verb => 'tweet', :object => 1}
+    end
 
-        before do
-            @client = Stream::Client.new('5crf3bhfzesn', 'tfq2sdqpj9g446sbv653x3aqmgn33hsn8uzdc9jpskaw8mj6vsnhzswuwptuj9su')
-            @feed42 = @client.feed('flat:42')
-            @test_activity = {:actor => 1, :verb => 'tweet', :object => 1}
-        end
+    context 'test client' do
 
         example "posting an activity" do
-            @feed42.add_activity(@test_activity)
+            response = @feed42.add_activity(@test_activity)
+            response.should include("id", "actor", "verb", "object", "target", "time")
+        end
+
+        example "posting and get one activity" do
+            response = @feed42.add_activity(@test_activity)
+            results = @feed42.get(:limit=>1)["results"]
+            results[0]["id"].should eq response["id"]
         end
 
         example "removing an activity" do
@@ -19,7 +26,7 @@ describe "Integration tests" do
             @feed42.remove(activity["id"])
         end
 
-        example "following a fed" do
+        example "following a feed" do
             @feed42.follow('flat:43')
         end
 
