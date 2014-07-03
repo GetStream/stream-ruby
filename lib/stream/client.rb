9cbc7e1e4f1e56d09048bd7455c3c902dda89029
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-07-03 11:37:08 +0200

    super happy

--- lib/stream/client.rb
+++ lib/stream/client.rb
@@ -3,10 +3,23 @@ require 'stream/feed'
 
 module Stream
     class Client
+        attr_reader :api_key
+        attr_reader :api_secret
+        attr_reader :site
 
-        def initialize(api_key, api_secret)
+        def initialize(api_key='', api_secret='', site=0)
+            # Support the Heroku STREAM_URL environment variable
+            
+            if ENV['STREAM_URL'] != nil and api_key == ''
+                matches = /https\:\/\/(?<key>\w+)\:(?<secret>\w+).*site=(?<site>\d+)/i.match(ENV['STREAM_URL'])
+                api_key = matches['key']
+                api_secret = matches['secret']
+                site = matches['site']
+            end
+          
             @api_key = api_key
             @api_secret = api_secret
+            @site = site
             @signer = Stream::Signer.new(api_secret)
         end
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,9 +5,29 @@ describe Stream::Client do
 
     it "feed returns a Feed instance with clean feed_id" do
         client = Stream::Client.new('key', 'secret')
+        client.api_key.should eq 'key'
+        client.api_secret.should eq 'secret'
         feed = client.feed('feed:42')
         expect(feed).to be_instance_of Stream::Feed
         feed.feed_id.should eq 'feed42'
+    end
+    
+    it "on heroku we connect using environment variables" do
+        ENV['STREAM_URL'] = 'https://thierry:pass@getstream.io/?site=1'
+        client = Stream::Client.new()
+        client.api_key.should eq 'thierry'
+        client.api_secret.should eq 'pass'
+        client.site.should eq '1'
+        ENV.delete 'STREAM_URL'
+    end
+    
+    it "but overwriting environment variables should be possible" do
+        ENV['STREAM_URL'] = 'https://thierry:pass@getstream.io/?site=1'
+        client = Stream::Client.new('1','2','3')
+        client.api_key.should eq '1'
+        client.api_secret.should eq '2'
+        client.site.should eq '3'
+        ENV.delete 'STREAM_URL'
     end
 
 end
