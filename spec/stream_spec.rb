b6793996ea94becb5082381cf0aba9788211153c
Ross Kaffenberger <rosskaff@gmail.com>
2015-10-26 18:23:21 -0400

    Enforce Style/ColonMethodCall

--- spec/stream_spec.rb
+++ spec/stream_spec.rb
@@ -3,7 +3,7 @@ require 'stream'
 describe Stream do
 
     it "connects returns a client instance" do
-        client = Stream::connect('key', 'secret')
+        client = Stream.connect('key', 'secret')
         expect(client).to be_instance_of Stream::Client
     end
 
